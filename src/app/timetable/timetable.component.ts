79c3977146c04988b34ab2b2229d5d760fdfc17c
AFNAN PC <mohammad afnan>
2022-05-20 03:32:38 +0500

    UI theming work 20 may 2022

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -51,12 +51,37 @@ export class TimetableComponent implements OnInit {
   // public currentView: View = 'Month';
   // public showWeekend: Boolean = true;
 
-  public resourceDataSource: Object[] = [
-    { Text: "green", Id: 1, Color: "#1aaa55" },
-    { Text: "blue", Id: 2, Color: "#357cd2" },
-    { Text: "red", Id: 3, Color: "#d0291e" },
-    { Text: "yellow", Id: 4, Color: "#e5e510" },
+  public resourceDataSourceType: Object[] = [
+    { Text: "Exam", Id: 1, Color: "#1aaa55" },
+    { Text: "Test", Id: 2, Color: "#357cd2" },
+    { Text: "Class", Id: 3, Color: "#d0291e" },
+    { Text: "Lab", Id: 4, Color: "#e5e510" },
   ];
+
+  public resourceDataSourceTeacher: Object[] = [
+    { Text: "tom", Id: 1 },
+    { Text: "danial", Id: 2 },
+    { Text: "john", Id: 3 },
+    { Text: "jeef", Id: 4 },
+    { Text: "adam", Id: 4 },
+  ];
+
+  public resourceDataSourceCourse: Object[] = [
+    { Text: "math", Id: 1 },
+    { Text: "physics", Id: 2 },
+    { Text: "chemistry", Id: 3 },
+    { Text: "english", Id: 4 },
+    { Text: "geaometry", Id: 5 },
+  ];
+
+  public resourceDataSourceRoom: Object[] = [
+    { Text: "Room 1", Id: 1 },
+    { Text: "Rom 2", Id: 2 },
+    { Text: "Room 3", Id: 3 },
+    { Text: "Room 4", Id: 4 },
+    { Text: "Room 5", Id: 5 },
+  ];
+
   // public group: GroupModel = { resources: ['Conferences'] };
   public eventSettings: EventSettingsModel = {
     // dataSource: this.data,
@@ -72,99 +97,70 @@ export class TimetableComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  onPopupOpen(args: PopupOpenEventArgs): void {
-    if (args.type === "Editor") {
-      if (!args.element.querySelector(".custom-field-row")) {
-        let row: HTMLElement = createElement("div", {
-          className: "custom-field-row",
-        });
-        let formElement: HTMLElement =
-          args.element.querySelector(".e-schedule-form");
-        formElement.firstChild.insertBefore(
-          row,
-          args.element.querySelector(".e-title-location-row")
-        );
-        let container: HTMLElement = createElement("div", {
-          className: "custom-field-container",
-        });
-        let inputEle: HTMLInputElement = createElement("input", {
-          className: "e-field",
-          attrs: { name: "EventType" },
-        }) as HTMLInputElement;
-        container.appendChild(inputEle);
-        row.appendChild(container);
-        let dropDownList: DropDownList = new DropDownList({
-          dataSource: [
-            { text: "Exam", value: "Exam" },
-            { text: "Test", value: "Test" },
-            { text: "Class", value: "Class" },
-            { text: "Lab", value: "Lab" },
-            { text: "Meeting", value: "Meeting" },
-            { text: "Result", value: "Result" },
-            { text: "Event", value: "Event" },
-          ],
-          fields: { text: "text", value: "value" },
-          value: (<{ [key: string]: Object }>args.data).EventType as string,
-          floatLabelType: "Always",
-          placeholder: "Type",
-        });
+  // onPopupOpen(args: PopupOpenEventArgs): void {
+  //   if (args.type === "Editor") {
+  //     if (!args.element.querySelector(".custom-field-row")) {
+  //       let row: HTMLElement = createElement("div", {
+  //         className: "custom-field-row",
+  //       });
+  //       let formElement: HTMLElement =
+  //         args.element.querySelector(".e-schedule-form");
+  //       formElement.firstChild.insertBefore(
+  //         row,
+  //         args.element.querySelector(".e-title-location-row")
+  //       );
+  //       let container: HTMLElement = createElement("div", {
+  //         className: "custom-field-container",
+  //       });
+  //       let inputEle: HTMLInputElement = createElement("input", {
+  //         className: "e-field",
+  //         attrs: { name: "EventType" },
+  //       }) as HTMLInputElement;
+  //       container.appendChild(inputEle);
+  //       row.appendChild(container);
+  //       let dropDownList: DropDownList = new DropDownList({
+  //         dataSource: [
+  //           { text: "Exam", value: "Exam" },
+  //           { text: "Test", value: "Test" },
+  //           { text: "Class", value: "Class" },
+  //           { text: "Lab", value: "Lab" },
+  //           { text: "Meeting", value: "Meeting" },
+  //           { text: "Result", value: "Result" },
+  //           { text: "Event", value: "Event" },
+  //         ],
+  //         fields: { text: "text", value: "value" },
+  //         value: (<{ [key: string]: Object }>args.data).EventType as string,
+  //         floatLabelType: "Always",
+  //         placeholder: "Type",
+  //       });
 
-        dropDownList.appendTo(inputEle);
-        inputEle.setAttribute("name", "EventType");
-      }
-    }
-    // if (args.type === "Editor") {
-    //   if (!args.element.querySelector(".custom-field-row")) {
-    //     let row: HTMLElement = createElement("div", {
-    //       className: "custom-field-row",
-    //     });
-    //     let formElement: HTMLElement =
-    //       args.element.querySelector(".e-schedule-form");
-    //     formElement.firstChild.insertBefore(
-    //       row,
-    //       args.element.querySelector(".e-title-location-row")
-    //     );
-    //     let container: HTMLElement = createElement("div", {
-    //       className: "custom-field-container",
-    //     });
-    //     let inputEle: HTMLInputElement = createElement("input", {
-    //       className: "e-field",
-    //       attrs: { name: "EventType" },
-    //     }) as HTMLInputElement;
+  //       dropDownList.appendTo(inputEle);
+  //       inputEle.setAttribute("name", "EventType");
+  //     }
+  //   }
 
-    //     new this.ej.inputs.Input.createInput({
-    //       element: inputEle,
-    //       floatLabelType: "Never",
-    //       readonly: true,
-    //       value: args.data.Customer_Name,
-    //       name: "Customer_Name",
-    //     });
-    //     container.appendChild(inputEle);
-    //     row.appendChild(container);
-    //   }
-    // }
+  // }
 
-    // template field
-    // onPopupOpen(args: PopupOpenEventArgs): void {
-    //   if (args.type === 'Editor') {
-    //     let statusElement: HTMLInputElement = args.element.querySelector('#EventType') as HTMLInputElement;
-    //     if (!statusElement.classList.contains('e-dropdownlist')) {
-    //       let dropDownListObject: DropDownList = new DropDownList({
-    //         placeholder: 'Choose Type', value: statusElement.value,
-    //         dataSource: ['Exam', 'Test', 'Class', 'Lab', 'Meeting', 'Result', 'Event']
-    //       });
-    //       dropDownListObject.appendTo(statusElement);
-    //       statusElement.setAttribute('name', 'EventType');
-    //     }
-    //     let startElement: HTMLInputElement = args.element.querySelector('#StartTime') as HTMLInputElement;
-    //     if (!startElement.classList.contains('e-datetimepicker')) {
-    //       new DateTimePicker({ value: new Date(startElement.value) || new Date() }, startElement);
-    //     }
-    //     let endElement: HTMLInputElement = args.element.querySelector('#EndTime') as HTMLInputElement;
-    //     if (!endElement.classList.contains('e-datetimepicker')) {
-    //       new DateTimePicker({ value: new Date(endElement.value) || new Date() }, endElement);
-    //     }
-    //   }
-    // }
-  }
+  // template field
+  // onPopupOpen(args: PopupOpenEventArgs): void {
+  //   if (args.type === 'Editor') {
+  //     let statusElement: HTMLInputElement = args.element.querySelector('#EventType') as HTMLInputElement;
+  //     if (!statusElement.classList.contains('e-dropdownlist')) {
+  //       let dropDownListObject: DropDownList = new DropDownList({
+  //         placeholder: 'Choose Type', value: statusElement.value,
+  //         dataSource: ['Exam', 'Test', 'Class', 'Lab',  'Result', ]
+  //       });
+  //       dropDownListObject.appendTo(statusElement);
+  //       statusElement.setAttribute('name', 'EventType');
+  //     }
+  //     let startElement: HTMLInputElement = args.element.querySelector('#StartTime') as HTMLInputElement;
+  //     if (!startElement.classList.contains('e-datetimepicker')) {
+  //       new DateTimePicker({ value: new Date(startElement.value) || new Date() }, startElement);
+  //     }
+  //     let endElement: HTMLInputElement = args.element.querySelector('#EndTime') as HTMLInputElement;
+  //     if (!endElement.classList.contains('e-datetimepicker')) {
+  //       new DateTimePicker({ value: new Date(endElement.value) || new Date() }, endElement);
+  //     }
+  //   }
+  // }
 }
