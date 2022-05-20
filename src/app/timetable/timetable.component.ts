afe72c1671a231a588fc4d14740337212f82a339
Muhammad Anas <muhammad.anas@muhammadanas.millsoft.com.pk>
2022-05-20 17:55:45 +0500

    Work By anas

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -13,6 +13,7 @@ import {
   AgendaService,
   ResizeService,
   PopupOpenEventArgs,
+  WorkHoursModel,
 } from "@syncfusion/ej2-angular-schedule";
 import { DropDownList } from "@syncfusion/ej2-dropdowns";
 import { createElement } from "@syncfusion/ej2-base";
@@ -45,72 +46,108 @@ L10n.load({
   ],
 })
 export class TimetableComponent implements OnInit {
+
   ej: any;
+  public showWeekend: boolean = false;
   public selectedDate: Date = new Date();
+  public scheduleHours: WorkHoursModel  = { highlight: true, start: '08:00', end: '20:00' };
 
-  // public currentView: View = 'Month';
-  // public showWeekend: Boolean = true;
-
-  public resourceDataSourceType: Object[] = [
-    { Text: "Exam", Id: 1, Color: "#e5b7e5" },
-    { Text: "Test", Id: 2, Color: "#a1ffe0" },
-    { Text: "Class", Id: 3, Color: "#b1b5ff" },
-    { Text: "Lab", Id: 4, Color: "#fffbe7" },
-  ];
-
-  public resourceDataSourceTeacher: Object[] = [
-    { Text: "Prof. Tom Hall", Id: 1 },
-    { Text: "Prof. John Den", Id: 2 },
-    { Text: "Prof. Daniel J. Post", Id: 3 },
-    { Text: "Prof. Walter M. Espinal", Id: 4 },
-    { Text: "Assistant Prof. Don S. Croteau", Id: 5 },
-  ];
-
-  public resourceDataSourceCourse: Object[] = [
-    { Text: "Introduction To Financial Accounting", Id: 1 },
-    { Text: "Probablity & Stats", Id: 2 },
-    { Text: "GeoEconomics", Id: 3 },
-    { Text: "Natural Language Processing", Id: 4 },
-    { Text: "Networking Data Layer", Id: 5 },
-  ];
-
-  public resourceDataSourceRoom: Object[] = [
-    { Text: "Room# 54", Id: 1 },
-    { Text: "Room# 55", Id: 2 },
-    { Text: "Room# 56", Id: 3 },
-    { Text: "Room# 57", Id: 4 },
-    { Text: "Room# 58", Id: 5 },
-    { Text: "Auditorium Hall", Id: 6 },
-    { Text: "Computational Lab# 1", Id: 7 },
-    { Text: "Computational Lab# 2", Id: 8 },
-  ];
-
-  // public group: GroupModel = { resources: ['Conferences'] };
-  public eventSettings: EventSettingsModel = {
-    // dataSource: this.data,
-    fields: {
-      subject: { title: "course", name: "course" },
-      location: { title: "course code", name: "course code" },
-      description: { title: "Summary", name: "Description" },
-      startTime: { title: "From", name: "StartTime" },
-      endTime: { title: "To", name: "EndTime" },
-      // course: { title: 'course ', name: "course" },
-    },
-  };
   constructor() { }
 
   ngOnInit() { }
+
+
+//   public eventSettings: EventSettingsModel = {
+//     dataSource: [
+//     {
+//         Id: 1,
+//         Subject: 'Explosion of Betelgeuse Star',
+//         StartTime: new Date(2018, 1, 15, 9, 30),
+//         EndTime: new Date(2018, 1, 15, 11, 0)
+//     }, {
+//         Id: 2,
+//         Subject: 'Thule Air Crash Report',
+//         StartTime: new Date(2018, 1, 12, 12, 0),
+//         EndTime: new Date(2018, 1, 12, 14, 0)
+//     }, {
+//         Id: 3,
+//         Subject: 'Blue Moon Eclipse',
+//         StartTime: new Date(2018, 1, 13, 9, 30),
+//         EndTime: new Date(2018, 1, 13, 11, 0)
+//     }, {
+//         Id: 4,
+//         Subject: 'Meteor Showers in 2018',
+//         StartTime: new Date(2018, 1, 14, 13, 0),
+//         EndTime: new Date(2018, 1, 14, 14, 30)
+//     }]
+// };
+
+  public resourceDataSourceType: Object[] = [
+    { name: "Exam", id: 1, Color: "#d93858" },
+    { name: "Test", id: 2, Color: "#fc6f9e" },
+    { name: "Class", id: 3, Color: "#525bfa" },
+    { name: "Lab", id: 4, Color: "#ada163" },
+  ];
+
+  public resourceDataSourceTeacher: Object[] = [
+    { name: "Prof. Tom Hall", id: 1 },
+    { name: "Prof. John Den", id: 2 },
+    { name: "Prof. Daniel J. Post", id: 3 },
+    { name: "Prof. Walter M. Espinal", id: 4 },
+    { name: "Assistant Prof. Don S. Croteau", id: 5 },
+  ];
+
+  public resourceDataSourceCourse: Object[] = [
+    { name: "Introduction To Financial Accounting", id: 1 },
+    { name: "Probablity & Stats", id: 2 },
+    { name: "GeoEconomics", id: 3 },
+    { name: "Natural Language Processing", id: 4 },
+    { name: "Networking Data Layer", id: 5 },
+  ];
+
+  public resourceDataClassGroup: Object[] = [
+    { name: "Group A", id: 1 },
+    { name: "Group B", id: 2 },
+    { name: "Group C", id: 3 },
+    { name: "Group D", id: 4 },
+    { name: "Group E", id: 5 },
+  ];
+
+  public resourceDataSourceRoom: Object[] = [
+    { name: "Room# 54", id: 1 },
+    { name: "Room# 55", id: 2 },
+    { name: "Room# 56", id: 3 },
+    { name: "Room# 57", id: 4 },
+    { name: "Room# 58", id: 5 },
+    { name: "Auditorium Hall", id: 6 },
+    { name: "Computational Lab# 1", id: 7 },
+    { name: "Computational Lab# 2", id: 8 },
+  ];
+
+  public eventSettings: EventSettingsModel = {
+    dataSource: [],
+    allowAdding:true,
+    allowDeleting:true,
+    allowEditing:true,
+  };
+
   onPopupOpen(args: PopupOpenEventArgs): void {
-    if (args.type === "Editor") {
+    console.log("DataSource : ",this.eventSettings.dataSource)
+    if (args.type === 'QuickInfo')  {
+      args.cancel = true;
+    }
+    else if (args.type === "Editor") {
       if (!args.element.querySelector('.custom-field-row')) {
-        this.createCustomDropdown(args, this.resourceDataSourceTeacher, "Text", "Id", "Teacher")
-        this.createCustomDropdown(args, this.resourceDataSourceRoom, "Text", "Id", "Room/Hall")
-        this.createCustomDropdown(args, this.resourceDataSourceCourse, "Text", "Id", "Course")
+        this.createCustomDropdown(args, this.resourceDataSourceType, "name", "id", "Type",false)
+        this.createCustomDropdown(args, this.resourceDataSourceTeacher, "name", "id", "Teacher",false)
+        this.createCustomDropdown(args, this.resourceDataSourceRoom, "name", "id", "Room/Hall",false)
+        this.createCustomDropdown(args, this.resourceDataSourceCourse, "name", "id", "Course",false)
+        this.createCustomDropdown(args, this.resourceDataClassGroup, "name", "id", "Group",false)
       }
     }
   }
 
-  createCustomDropdown(args, dataSource, propName1, propName2, dropDownName) {
+  createCustomDropdown(args, dataSource, propName1, propName2, dropDownName,isPopupReadOnly) {
     let row: HTMLElement = createElement("div", {
       className: "custom-field-row",
     });
@@ -130,15 +167,15 @@ export class TimetableComponent implements OnInit {
     container.appendChild(inputEle);
     row.appendChild(container);
     let dropDownList: DropDownList = new DropDownList({
+      readonly:isPopupReadOnly,
       dataSource: dataSource,
-      fields: { text: propName1, value: propName2 },
+      fields: { text: propName1, value: propName2, },
       value: (<{ [key: string]: Object }>args.data).EventType as string,
       floatLabelType: "Always",
       placeholder: dropDownName,
     });
-
     dropDownList.appendTo(inputEle);
-    inputEle.setAttribute("name", "EventType");
+    inputEle.setAttribute("name", dropDownName);
   }
 
 
