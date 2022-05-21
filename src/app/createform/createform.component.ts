c6c0effc0a30af002003c41067a4e5fda1ed7d26
Afnan <mohammad.afnan@millsoft.com.pk>
2022-05-21 14:01:15 +0500

    timetable services added 21 may 2022

--- src/app/createform/createform.component.ts
+++ src/app/createform/createform.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { GlobalService } from "../service/global.service";
 
 @Component({
   selector: "app-createform",
@@ -7,16 +8,26 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
   styleUrls: ["./createform.component.css"],
 })
 export class CreateformComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<CreateformComponent>) {}
+  constructor(public dialogRef: MatDialogRef<CreateformComponent>, public globalService: GlobalService) { }
 
   show: string = "teacher";
 
-  ngOnInit() {}
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+  ngOnInit() { }
+  // onNoClick(): void {
+  //   this.dialogRef.close();
+  // }
 
   gotoform(text) {
     this.show = text;
+
+  }
+
+  getCurrentData() {
+    if (this.show == "teacher") {
+      return this.globalService.teacherData
+    }
+    else if (this.show == "student") {
+      // return this.globalService.studentData
+    }
   }
 }
---
+++ src/app/service/global.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Class } from '../models/class';
+import { Course } from '../models/course';
+import { Group } from '../models/group';
+import { Student } from '../models/student';
+import { Teacher } from '../models/teacher';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+
+  teacherData: Teacher[] = [];
+  StudentData: Student[] = [];
+  courseData: Course[] = [];
+  classData: Class[] = [];
+  groupData: Group[] = [];
+
+
+  constructor() { }
+}
